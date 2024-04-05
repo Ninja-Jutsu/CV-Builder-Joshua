c11376908d33f67659e7514e2b7946fb72822895
Ninja-Jutsu <ninja.jutsu.way@gmail.com>
2024-04-06 00:01:21 +0800

    Added Section for experience and education

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import Header from '../Header/Header';
 import Main from '../Main/Main';
-import SideBar from '../SideBar/SideBar';
-import RefProvider from '../RefProvider/RefProvider';
-
+import Footer from '../Footer/Footer';
 function App() {
   return (
-    <RefProvider>
+    <>
       <Header />
       <Main />
-      <SideBar />
-    </RefProvider>
+      <Footer />
+    </>
   );
 }
 
